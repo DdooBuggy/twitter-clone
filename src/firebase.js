2400e5bdbd5b49d47646a09315f213682e9167b5
DdooBuggy <yhiyo03@gmail.com>
2022-07-03 18:52:06 +0900

    env file for securing the keys

--- src/firebase.js
+++ src/firebase.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCGps13Bej46BWDi9MyVn8PIhDCgQD5mUw",
-  authDomain: "twitter-clone-course-ba9a9.firebaseapp.com",
-  projectId: "twitter-clone-course-ba9a9",
-  storageBucket: "twitter-clone-course-ba9a9.appspot.com",
-  messagingSenderId: "607405810630",
-  appId: "1:607405810630:web:82638fe6db10ed8abc48c7",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGEING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 // Initialize Firebase
