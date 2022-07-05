33aebf46368120343f9736dee50158d7cd4e4e9a
DdooBuggy <yhiyo03@gmail.com>
2022-07-05 20:07:28 +0900

    getting the nweets

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,9 +1,24 @@
-import React, { useState } from "react";
-import { collection, addDoc } from "firebase/firestore";
+import React, { useEffect, useState } from "react";
+import { collection, addDoc, getDocs } from "firebase/firestore";
 import { dbService } from "../fbase";
 
 const Home = () => {
-  const [nweet, setNweet] = useState("");
+  const [nweet, setNweet] = useState(""); // nweet form value
+  const [nweets, setNweets] = useState([]); // nweets in database
+  const getNweets = async () => {
+    try {
+      const querySnapshot = await getDocs(collection(dbService, "nweet"));
+      querySnapshot.forEach((doc) => {
+        const nweetObj = { ...doc.data(), id: doc.id };
+        setNweets((prev) => [nweetObj, ...prev]);
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    getNweets();
+  }, []);
   const onSubmit = async (event) => {
     event.preventDefault();
     try {
@@ -34,6 +49,13 @@ const Home = () => {
         />
         <input type="submit" value="Nweet" />
       </form>
+      <div>
+        {nweets.map((nweet) => (
+          <div key={nweet.id}>
+            <h4>{nweet.nweet}</h4>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
