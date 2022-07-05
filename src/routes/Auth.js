e0ab3616ebb3a4ce913fd111c8bfd308cba34646
DdooBuggy <yhiyo03@gmail.com>
2022-07-05 17:37:27 +0900

    social login

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,8 +1,11 @@
 import React, { useState } from "react";
 import {
   getAuth,
+  signInWithPopup,
+  GoogleAuthProvider,
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
+  GithubAuthProvider,
 } from "firebase/auth";
 
 const Auth = () => {
@@ -42,6 +45,24 @@ const Auth = () => {
     }
   };
   const toggleAccount = () => setNewAccount((prev) => !prev);
+  const onSocialClick = async (event) => {
+    const {
+      target: { name },
+    } = event;
+    let provider;
+    if (name === "google") {
+      provider = new GoogleAuthProvider();
+    } else if (name === "github") {
+      provider = new GithubAuthProvider();
+    } else {
+      return;
+    }
+    try {
+      const result = await signInWithPopup(auth, provider);
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <div>
       <form onSubmit={onSubmit}>
@@ -68,8 +89,12 @@ const Auth = () => {
         {newAccount ? "Sign In" : "Create Account"}
       </div>
       <div>
-        <button>Continue with Google</button>
-        <button>Continue with Github</button>
+        <button onClick={onSocialClick} name="google">
+          Continue with Google
+        </button>
+        <button onClick={onSocialClick} name="github">
+          Continue with Github
+        </button>
       </div>
     </div>
   );
