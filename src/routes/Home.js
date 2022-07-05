c21c888500ba8f0b7c018b53fe6d6a807e9f317c
DdooBuggy <yhiyo03@gmail.com>
2022-07-05 18:55:51 +0900

    home tweet form

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,7 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Home = () => {
-  return <h2>home</h2>;
+  const [nweet, setNweet] = useState("");
+  const onSubmit = (event) => {
+    event.preventDefault();
+  };
+  const onChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setNweet(value);
+  };
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          value={nweet}
+          onChange={onChange}
+          type="text"
+          placeholder="What's on your mind?"
+          maxLength={120}
+        />
+        <input type="submit" value="Nweet" />
+      </form>
+    </div>
+  );
 };
 
 export default Home;
